ab290b268957a9c26503adceb47661fe6c22a8e4
Goodkhau <gos.khai12@gmail.com>
2025-07-21 19:21:00 -0400

    Added Message model

--- firstproject/base/models.py
+++ firstproject/base/models.py
@@ -13,3 +13,14 @@ class Room(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Message(models.Model):
+    #user = 
+    room = models.ForeignKey(Room)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[0:50]

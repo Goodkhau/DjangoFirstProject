3d8ba81175abaf46f18fe6cc04d4d7f41d0ecde1
Goodkhau <gos.khai12@gmail.com>
2025-07-21 19:26:36 -0400

    Added users to Message DB and topic model

--- firstproject/base/models.py
+++ firstproject/base/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -15,9 +16,16 @@ class Room(models.Model):
         return self.name
 
 
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
 class Message(models.Model):
-    #user = 
-    room = models.ForeignKey(Room)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
     updated = models.DateTimeField(auto_now=True)
     updated = models.DateTimeField(auto_now_add=True)

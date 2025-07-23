fb35edb8257dc7faf3828991421c85f46c173361
Goodkhau <gos.khai12@gmail.com>
2025-07-22 20:51:19 -0400

    Updated createRoom to save the form and redirect

--- firstproject/base/urls.py
+++ firstproject/base/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="Home"),
+    path('', views.home, name="home"),
     path('room/<str:pk>/', views.room, name="room"),
     path('create-room/', views.createRoom, name="create-room"),
 ]

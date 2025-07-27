c088080ebc28ebbec8a9655b2e319d26238a4f39
Goodkhau <gos.khai12@gmail.com>
2025-07-27 19:18:09 -0400

    Added different qualities to filter rooms

--- firstproject/base/views.py
+++ firstproject/base/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from django.db.models import Q
 from .forms import RoomForm
-from .models import Topic
+from .models import Room, Topic
 
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
 
-    rooms = Room.objects.filter(topic__name__icontains=q)
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) | 
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
 
     topics = Topic.objects.all()
 
